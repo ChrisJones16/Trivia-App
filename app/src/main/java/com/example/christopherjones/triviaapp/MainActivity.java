3dc362c87345fd68c4c46834da84d697b9f3218e
Christopher Jones <cmjones751@gmail.com>
2018-06-19 14:16:14 -0400

    Created Fragment Holder

--- app/src/main/java/com/example/christopherjones/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/christopherjones/triviaapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.christopherjones.triviaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionCreatorFragment questionCreatorFragment;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.add_question_button)
+    protected void addquestionClicked() {
+
+       questionCreatorFragment = QuestionCreatorFragment.newInstance();
+
+       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+
+    }
+
 }

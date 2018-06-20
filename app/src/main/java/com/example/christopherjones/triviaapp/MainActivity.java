21c86186c9db4f2d16fbe8c1fce951b2bc2500d5
Christopher Jones <cmjones751@gmail.com>
2018-06-20 15:41:32 -0400

    Created Logic in Main Activity

--- app/src/main/java/com/example/christopherjones/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/christopherjones/triviaapp/MainActivity.java
@@ -4,14 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements QuestionCreatorFragment.Callback{
 
     private QuestionCreatorFragment questionCreatorFragment;
 
-
+    private List<Question>questionList;
 
 
     @Override
@@ -19,15 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        questionList = new ArrayList<>();
     }
 
     @OnClick(R.id.add_question_button)
     protected void addquestionClicked() {
 
        questionCreatorFragment = QuestionCreatorFragment.newInstance();
-
+       questionCreatorFragment.attachParent(this);
        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
 
     }
 
+    @Override
+    public void questionSaved(Question question) {
+        //Takes the question object that was passed in and saves it to the question ArrayList
+        questionList.add(question);
+        //Shows a Toast to the user that lets them know the question was saved.
+        Toast.makeText(this, "Question Saved", Toast.LENGTH_SHORT).show();
+        //Removes fragment from the frame layout
+        getSupportFragmentManager().beginTransaction().remove(questionCreatorFragment).commit();
+    }
 }
--- app/src/main/java/com/example/christopherjones/triviaapp/QuestionCreatorFragment.java
+++ app/src/main/java/com/example/christopherjones/triviaapp/QuestionCreatorFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,20 +20,18 @@ public class QuestionCreatorFragment extends Fragment {
     protected EditText question;
 
     @BindView(R.id.correct_answer_editext)
-    protected EditText correctAnswer;
+    protected EditText correctAnswerInput;
 
     @BindView(R.id.first_wrong_answer_editext)
-    protected EditText firstWrongAnswer;
+    protected EditText firstWrongAnswerInput;
 
     @BindView(R.id.second_wrong_answer_editext)
-    protected EditText secondWrongAnswer;
+    protected EditText secondWrongAnswerInput;
 
     @BindView(R.id.third_wrong_answer_editext)
-    protected EditText thirdWrongAnswer;
+    protected EditText thirdWrongAnswerInput;
 
-
-
-
+    private Callback callback;
 
 
     @Nullable
@@ -56,9 +55,34 @@ public class QuestionCreatorFragment extends Fragment {
     @OnClick(R.id.save_question_button)
     protected void addQuestion() {
 
-        String questionTitle = question.getText().toString();
-        String correct
+        if (question.getText().toString().isEmpty() || correctAnswerInput.getText().toString().isEmpty() || secondWrongAnswerInput.getText().toString().isEmpty()
+                || thirdWrongAnswerInput.getText().toString().isEmpty()) {
 
+            Toast.makeText(getActivity(), "All fields are required", Toast.LENGTH_SHORT).show();
+        } else {
+
+            String questionTitle = question.getText().toString();
+            String correctAnswer = this.correctAnswerInput.getText().toString();
+            String firstWrongAnswer = this.firstWrongAnswerInput.getText().toString();
+            String secondWrongAnswer = this.secondWrongAnswerInput.getText().toString();
+            String thirdWrongAnswer = this.thirdWrongAnswerInput.getText().toString();
+
+            //takes variables created from user input and saves them in Question Object
+            Question question = new Question(questionTitle, correctAnswer, firstWrongAnswer, secondWrongAnswer, thirdWrongAnswer);
+
+            //sends question object we just created to the callback method to be saved.
+            callback.questionSaved(question);
+        }
+    }
+
+    public void attachParent(Callback callback) {
+        this.callback = callback;
+    }
+
+    public interface Callback {
+
+        void questionSaved(Question question);
     }
 
 }
+
